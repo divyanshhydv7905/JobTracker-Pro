7e2d4f8e19a53ba7616b18422a4bded95f619e95
divyanshhydv7905 <dyadav.9648@gmail.com>
2026-08-13 23:41:05 +0530

    DAY 2

--- server/server.js
+++ server/server.js
@@ -1,2 +1,31 @@
 const express = require("express");
+
 const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("JobTrack Pro Server is working!");
+});
+
+app.get("/jobs", (req, res) => {
+    res.send("All Jobs");
+});
+
+app.post("/jobs", (req, res) => {
+    console.log(req.body);
+
+    res.send("Job added successfully");
+});
+
+const server = app.listen(5000, () => {
+    console.log("Server started!");
+});
+
+server.on("error", (error) => {
+    console.log("SERVER ERROR:", error);
+});
+
+server.on("close", () => {
+    console.log("SERVER CLOSED!");
+});
